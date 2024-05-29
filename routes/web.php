0bbe081868430ae406c69e54d10f9909a804dcc5
faidhol anwar <faidholanwar17@gmail.com>
2024-05-29 15:06:46 +0700

    menambah component

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Beranda');
 });
 Route::get('/Beranda', function () {
     return view('Beranda');
